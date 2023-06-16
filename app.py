17e028470f057e315b228edf57d2eb4eb24afded
jiayii01 <79521323+jiayii01@users.noreply.github.com>
2023-06-16 13:12:28 +0800

    adding all related files

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+st.title("Tensorflow Intel Image Classification")
+
+@st.cache_resource()
+def load_model():
+    model = tf.keras.models.load_model('intel_image_model.hdf5')
+    return model
+
+HEIGHT = 224
+WIDTH = 224
+def preprocess(img, label):
+    return tf.image.resize(img, [HEIGHT, WIDTH]) / 255, label
+
+def predict_class(image, model):
+#     image.map(preprocess)
+    
+#     prediction = model.predict(image)
+#     return prediction
+
+    image = tf.cast(image, tf.float32)
+    image = tf.image.resize(image, [HEIGHT, WIDTH]) / 255
+    image = np.expand_dims(image, axis = 0)
+
+    prediction = model.predict(image)
+    return prediction
+
+model = load_model()
+
+img_file = st.file_uploader(
+    "Upload an image of buildings, forests, glaciers, mountains, sea or streets.", type=["jpg", "jpeg", "png"])
+
+if img_file:
+    slot = st.empty()
+    with st.spinner("Processing Image..."):
+        image = Image.open(img_file)
+        st.image(image, caption="Input Image", width = 400)
+
+        pred = predict_class(np.asarray(image), model)
+                
+        class_names = ['buildings', 'forests', 'glaciers', 'mountains', 'sea', 'streets']
+        
+        result = class_names[np.argmax(pred)]
+                
+        output = 'The image is of ' + result
+
+        st.success(output)
+else:
+    st.text('Waiting for an image upload')
